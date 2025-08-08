2ba93b6d4d05bef0472415b673ceb28b68be8c7f
Shibam57 <shibamsamantascience@gmail.com>
2025-08-09 00:40:35 +0530

    my first mern project

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,7 +25,7 @@ app.use('/v1/users', userRouter);
 app.use(express.static(path.join(__dirname, '/dist')));
 
 app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname,  '/dist/index.html'));
+    res.sendFile(path.join(__dirname,  '/dist/', 'index.html'));
 });
 
 module.exports = app;
