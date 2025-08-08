f09335640d29d1bc533729d2a4633884aaf6b7ba
Shibam57 <shibamsamantascience@gmail.com>
2025-08-08 23:57:51 +0530

    my first mern project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
