b8a470da5e5022df329186343b593a2131f1a549
Shibam57 <shibamsamantascience@gmail.com>
2025-08-09 10:45:20 +0530

    first

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,7 +28,7 @@ app.use('/v1/users', userRouter);
 app.use(express.static(path.join(__dirname, '../../frontend', 'dist')));
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../../frontend', 'dist', 'index.html'));
 });
 
 module.exports = app;
