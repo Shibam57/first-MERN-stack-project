a3d43a3d7891d042524ccd9db87ce17f74069d07
Shibam57 <shibamsamantascience@gmail.com>
2025-08-09 10:06:27 +0530

    first

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,14 +10,25 @@ app.use(cors({
     credentials: true
 }));
 
-// These are for JSON/URL-encoded, not for multipart
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb'  }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
 
+
+
 const userRouter = require('./routes/userRoute.js');
 app.use('/v1/users', userRouter); 
+
+
+
+
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
+});
 
 module.exports = app;
